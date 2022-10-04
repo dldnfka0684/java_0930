a872af83280b4a603851678e1b26398fae1b40df
tjoeun <tjoeun@DESKTOP-PA5EOVA>
2022-10-04 18:23:28 +0900

    1004

---
+++ java_1004/src/java_1004/Unary_1.java
@@ -0,0 +1,24 @@
+package java_1004;
+
+public class Unary_1 {
+	public static void main(String[] args) {
+		
+		int x = 1;
+		switch(x){
+			case 1:
+				System.out.println("일");
+				int k = 10;
+			
+			case 2:
+				System.out.println("이");
+				break;
+		}
+
+	    
+		}
+}
+
+		
+
+	
+
